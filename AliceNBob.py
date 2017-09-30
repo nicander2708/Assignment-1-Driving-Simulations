16ea2436077d284be36d94fddfb48541ede14db9
nicander2708 <nicander_ac@yahoo.com>
2017-09-30 10:10:20 +0700

    Assignment 2

--- AliceNBob.py
+++ AliceNBob.py
@@ -1,6 +1,9 @@
-stone = int(input())
-if stone%2 ==1:
-     print('ALICE')
-elif stone%2 ==0:
-     print('BOB')
-# Alice will be printed if input is odd, and Bob will be printed if input is even
+def alicenbob():
+
+     stone = int(input())
+     if stone%2 ==1:
+          print('ALICE')
+     elif stone%2 ==0:
+          print('BOB')
+     # Alice will be printed if input is odd, and Bob will be printed if input is even
+alicenbob()
--- kmp.py
+++ kmp.py
@@ -1,6 +1,9 @@
-inp = input().title()# input the name with all lowercase is also ok since .title() is used
-result = ''
-for x in inp.split('-'):
-    result += x[0]
-print(result)
+def kmp():
 
+    inp = input().title()# input the name with all lowercase is also ok since .title() is used
+    result = ''
+    for x in inp.split('-'):
+        result += x[0]
+    print(result)
+
+kmp()
